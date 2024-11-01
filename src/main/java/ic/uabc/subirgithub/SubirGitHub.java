853e9416fca17723828ddac8e786d01c066170ea
victor18006 <vosorio@uabc.edu.mx>
2024-11-01 13:07:08 -0700

    V1.0 Primero

---
+++ src/main/java/ic/uabc/subirgithub/SubirGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ic.uabc.subirgithub;
+
+/**
+ *
+ * @author Hp
+ */
+public class SubirGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
